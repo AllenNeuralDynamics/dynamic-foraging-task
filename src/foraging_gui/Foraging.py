c618f1291ddc993e5966e115a8d2c26c795a89d2
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2023-03-08 11:30:59 -0800

    rearranged main code

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -3,6 +3,7 @@ import numpy as np
 from datetime import date,timedelta,datetime
 from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox,QFileDialog,QVBoxLayout
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import QThreadPool
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from scipy.io import savemat, loadmat
 from ForagingGUI import Ui_ForagingGUI
@@ -11,11 +12,10 @@ from MotorStage import Ui_MotorStage
 from Manipulator import Ui_Manipulator
 import rigcontrol
 from pyOSC3.OSC3 import OSCStreamingClient
-from PyQt5.QtCore import *
 from Visualization import PlotV
 from Dialogs import OptogeneticsDialog
 from Dialogs import WaterCalibrationDialog
-from MyFunctions import GenerateTrials
+from MyFunctions import GenerateTrials, Worker
 #import subprocess
 #import h5py
 #from scipy import stats
@@ -439,75 +439,6 @@ class MotorStageDialog(QDialog,Ui_MotorStage):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-
-
-class WorkerSignals(QObject):
-    '''
-    Defines the signals available from a running worker thread.
-
-    Supported signals are:
-
-    finished
-        No data
-
-    error
-        tuple (exctype, value, traceback.format_exc() )
-
-    result
-        object data returned from processing, anything
-
-    progress
-        int indicating % progress
-
-    '''
-    finished = pyqtSignal()
-    error = pyqtSignal(tuple)
-    result = pyqtSignal(object)
-    progress = pyqtSignal(int)
-
-
-class Worker(QRunnable):
-    '''
-    Worker thread
-
-    Inherits from QRunnable to handler worker thread setup, signals and wrap-up.
-
-    :param callback: The function callback to run on this worker thread. Supplied args and
-                     kwargs will be passed through to the runner.
-    :type callback: function
-    :param args: Arguments to pass to the callback function
-    :param kwargs: Keywords to pass to the callback function
-
-    '''
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
-
-        # Store constructor arguments (re-used for processing)
-        self.fn = fn
-        self.args = args
-        self.kwargs = kwargs
-        self.signals = WorkerSignals()
-        self.setAutoDelete(False) 
-        # Add the callback to our kwargs
-        #self.kwargs['progress_callback'] = self.signals.progress
-
-    @pyqtSlot()
-    def run(self):
-        '''
-        Initialise the runner function with passed args, kwargs.
-        '''
-
-        # Retrieve args/kwargs here; and fire processing using them
-        try:
-            result = self.fn(*self.args, **self.kwargs)
-        except:
-            traceback.print_exc()
-            exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-        else:
-            self.signals.result.emit(result)  # Return the result of the processing
-        finally:
-            self.signals.finished.emit()  # Done
 
 
 if __name__ == "__main__":
