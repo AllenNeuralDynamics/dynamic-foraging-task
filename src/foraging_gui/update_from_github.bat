d3b2fc2a6296b8f5aeff41a6ef587d11efe16d56
Alex Piet <alex.piet@alleninstitute.org>
2023-11-27 15:14:46 -0800

    testing git reset branch

--- src/foraging_gui/update_from_github.bat
+++ src/foraging_gui/update_from_github.bat
@@ -3,6 +3,7 @@ set "repo=C:\Users\alex.piet\dynamic-foraging-task"
 cd %repo%
 echo dynamic-foraging-gui update >>%logfile%
 echo %date% >>%logfile%
+git stash >>%logfile% 2>&1
 ::git checkout main >>%logfile% 2>&1
 ::git reset --hard >>%logfile% 2>&1
 ::git pull origin main >>%logfile% 2>&1
