026b4c2428419216a5e44c61a66fee64cb1cc2d6
Alex Piet <alex.piet@alleninstitute.org>
2023-11-27 15:40:25 -0800

    adding branch as variable

--- src/foraging_gui/update_from_github_to_main.bat
+++ src/foraging_gui/update_from_github_to_main.bat
@@ -1,10 +1,11 @@
 set "logfile=C:\Users\alex.piet\foraging_gui_logs\github_log.txt"
 set "repo=C:\Users\alex.piet\dynamic-foraging-task"
+set "branch=fix_61"
 cd %repo%
-echo dynamic-foraging-gui update >>%logfile%
+echo dynamic-foraging-gui update to %branch% >>%logfile%
 echo %date% >>%logfile%
 git stash >>%logfile% 2>&1
-git checkout fix_61 >>%logfile% 2>&1
+git checkout %branch% >>%logfile% 2>&1
 git reset --hard >>%logfile% 2>&1
-git pull origin fix_61 >>%logfile% 2>&1
+git pull origin %branch% >>%logfile% 2>&1
 echo --------------------------- >>%logfile%
