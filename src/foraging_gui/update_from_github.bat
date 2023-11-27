55846b74bf73016464bc6c12b623158bb7b0b886
Alex Piet <alex.piet@alleninstitute.org>
2023-11-27 15:12:44 -0800

    testing

--- src/foraging_gui/update_from_github.bat
+++ src/foraging_gui/update_from_github.bat
@@ -3,7 +3,7 @@ set "repo=C:\Users\alex.piet\dynamic-foraging-task"
 cd %repo%
 echo dynamic-foraging-gui update >>%logfile%
 echo %date% >>%logfile%
-::git checkout main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
-::git reset --hard >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
-::git pull origin main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
+::git checkout main >>%logfile% 2>&1
+::git reset --hard >>%logfile% 2>&1
+::git pull origin main >>%logfile% 2>&1
 echo --------------------------- >>%logfile%
