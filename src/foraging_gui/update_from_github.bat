7c2de2fc2cc3a92dee3ca33d9929a843561e57b3
Alex Piet <alex.piet@alleninstitute.org>
2023-11-27 15:06:58 -0800

    updating file

--- src/foraging_gui/update_from_github.bat
+++ src/foraging_gui/update_from_github.bat
@@ -1,7 +1,8 @@
+set "logfile=C:\Users\alex.piet\foraging_gui_logs\github_log.txt"
 cd C:\Users\svc_aind_behavior\dynamic-foraging-task
-echo dynamic-foraging-gui update >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt
-echo %date% >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt
-git checkout main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
-git reset --hard >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
-git pull origin main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
-echo --------------------------- >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt
+echo dynamic-foraging-gui update >>%logfile%
+echo %date% >>%logfile%
+::git checkout main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
+::git reset --hard >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
+::git pull origin main >>C:\Users\svc_aind_behavior\foraging_gui_logs\github_log.txt 2>&1
+echo --------------------------- >>%logfile%
