07949ade71ceaa7bfc527d548a6deb1b7547c23c
Alex Piet <alex.piet@alleninstitute.org>
2023-11-27 15:17:58 -0800

    testing reset

--- src/foraging_gui/update_from_github.bat
+++ src/foraging_gui/update_from_github.bat
@@ -5,6 +5,6 @@ echo dynamic-foraging-gui update >>%logfile%
 echo %date% >>%logfile%
 git stash >>%logfile% 2>&1
 git checkout main >>%logfile% 2>&1
-::git reset --hard >>%logfile% 2>&1
-::git pull origin main >>%logfile% 2>&1
+git reset --hard >>%logfile% 2>&1
+git pull origin main >>%logfile% 2>&1
 echo --------------------------- >>%logfile%
