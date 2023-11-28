e7f5af2e0dda1b29c04d55572d87c98621b5804f
Alex Piet <alexpiet@gmail.com>
2023-11-28 12:40:38 -0800

    adding git fetch

--- src/foraging_gui/update_from_github_to_main.bat
+++ src/foraging_gui/update_from_github_to_main.bat
@@ -2,10 +2,14 @@ set "logfile=C:\Users\alex.piet\Documents\foraging_gui_logs\github_log.txt"
 set "repo=C:\Users\alex.piet\dynamic-foraging-task"
 set "branch=fix_61"
 cd %repo%
-echo dynamic-foraging-gui update to %branch% >>%logfile%
+echo --------------------------- >>%logfile%
 echo %date% >>%logfile%
+echo dynamic-foraging-gui update to %branch% >>%logfile%
+echo running git fetch >>%logfile%
+git fetch origin main >>%logfile% 2>&1
+git fetch origin %branch% >>%logfile% 2>&1
+echo running git stash, and changing branch >>%logfile%
 git stash >>%logfile% 2>&1
 git checkout %branch% >>%logfile% 2>&1
 git reset --hard >>%logfile% 2>&1
 git pull origin %branch% >>%logfile% 2>&1
-echo --------------------------- >>%logfile%
