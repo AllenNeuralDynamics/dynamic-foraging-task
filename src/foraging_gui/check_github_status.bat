202756098f755b148f5e69734d80396f819e125e
Alex Piet <alexpiet@gmail.com>
2023-12-05 11:04:22 -0800

    adding script for logging status of testing computer

---
+++ src/foraging_gui/check_github_status.bat
@@ -0,0 +1,8 @@
+set "logfile=C:\Users\svc_aind_behavior\Documents\foraging_gui_logs\github_log.txt"
+set "repo=C:\Users\svc_aind_behavior\dynamic-foraging-task"
+cd %repo%
+echo --------------------------- >>%logfile%
+echo %date% %time% >>%logfile%
+echo status check for testing rig >>%logfile%
+git status >>%logfile% 2>&1
+
