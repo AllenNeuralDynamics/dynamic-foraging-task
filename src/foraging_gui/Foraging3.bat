2e401a7d0dbb265a86219cbecc40d43930c88289
Alex Piet <alexpiet@gmail.com>
2023-12-12 12:32:08 -0800

    updating conda environment and path to repo installation

--- src/foraging_gui/Foraging3.bat
+++ src/foraging_gui/Foraging3.bat
@@ -1,3 +1,3 @@
-cd /d E:\GitHub\dynamic-foraging-task\src\foraging_gui
+cd /d C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
 call conda activate Foraging
 start python Foraging.py 3
