00998ff6fb98916a15e1310f9849205a8a987af1
Alex Piet <alexpiet@gmail.com>
2024-01-12 12:45:47 -0800

    adding bonsai path

--- src/desktop_shortcuts/CameraA.bat
+++ src/desktop_shortcuts/CameraA.bat
@@ -1,8 +1,7 @@
 cd /d C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\workflows\Cameras
 set "logfile=C:\Users\svc_aind_behavior\Documents\foraging_gui_logs\bonsai_log.txt"
-start "" pythonw Foraging.py 1
 echo --------------------------- >>%logfile%
 echo %date% %time% >>%logfile%
-start bonsai Camera_boxA.bonsai --no-editor >>%logfile% 2>&1
+start C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxA.bonsai --no-editor >>%logfile% 2>&1
 
 
