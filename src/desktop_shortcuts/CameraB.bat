f77ab1f9e38654f29d1af7d43df9cd8589c97f31
Alex Piet <alexpiet@gmail.com>
2024-01-12 15:33:21 -0800

    setting window to minimize

--- src/desktop_shortcuts/CameraB.bat
+++ src/desktop_shortcuts/CameraB.bat
@@ -1,3 +1,3 @@
 cd /d C:\Users\svc_aind_behavior\Documents\camera_workflows
-start "" C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxB.bonsai --no-editor 
+start /min C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxB.bonsai --no-editor 
 
