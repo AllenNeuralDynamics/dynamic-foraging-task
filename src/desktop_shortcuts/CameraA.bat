d63974b5f6c0327af9c25382747712d0905eeec5
Alex Piet <alexpiet@gmail.com>
2024-01-12 13:20:10 -0800

    changin defaults

--- src/desktop_shortcuts/CameraA.bat
+++ src/desktop_shortcuts/CameraA.bat
@@ -1,7 +1,7 @@
-cd /d C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\workflows\Cameras
+cd /d C:\Users\svc_aind_behavior\Documents\camera_workflows
 set "logfile=C:\Users\svc_aind_behavior\Documents\foraging_gui_logs\bonsai_log.txt"
 echo --------------------------- >>%logfile%
 echo %date% %time% >>%logfile%
-start C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxA.bonsai --no-editor >>%logfile% 2>&1
+start "" C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxA.bonsai --no-editor >>%logfile% 2>&1
 
 
