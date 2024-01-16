c42de5b1e6f63c84fc86bc31c43ac13e626f4234
Alex Piet <alexpiet@gmail.com>
2024-01-16 15:34:31 -0800

    testing

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2154,7 +2154,7 @@ class Window(QMainWindow):
 
     def _CheckBonsaiConnection(self):
         '''Check if the Bonsai and GUI are connected'''
-        self._ConnectBonsai()
+        self._ConnectOSC()
         #if self.InitializeBonsaiSuccessfully==0:
         #    self._ConnectBonsai()
         
