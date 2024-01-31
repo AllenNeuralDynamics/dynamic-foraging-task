8ede33d9b96b497ee105def0aa9b95bb958a13ae
Alex Piet <alexpiet@gmail.com>
2024-01-31 12:14:11 -0800

    grabbing username, instead of assuming svc_aind_behavior

--- src/desktop_shortcuts/BoxB with console.bat
+++ src/desktop_shortcuts/BoxB with console.bat
@@ -1,4 +1,4 @@
-cd /d C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
+cd /d C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
 call conda activate Foraging
 python Foraging.py 2
 timeout 3600 > NUL
--- src/desktop_shortcuts/BoxD.bat
+++ src/desktop_shortcuts/BoxD.bat
@@ -1,6 +1,6 @@
-cd /d C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
+cd /d C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
 call conda activate Foraging
-start "" C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\src\desktop_shortcuts\start_popup.bat
+start "" C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\desktop_shortcuts\start_popup.bat
 powershell -window minimized -command ""
 python Foraging.py 4 --no-bonsai-ide
 timeout 3600 > NUL
