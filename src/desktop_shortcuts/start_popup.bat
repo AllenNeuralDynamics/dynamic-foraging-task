ac282e9c98631277aabaddc9ca0f16b2b3a06d81
Alex Piet <alexpiet@gmail.com>
2024-01-31 11:32:16 -0800

    cleaning up popup window

--- src/desktop_shortcuts/start_popup.bat
+++ src/desktop_shortcuts/start_popup.bat
@@ -1,20 +1,22 @@
 echo off
-mode 20,30
+mode 70,19
 cls
-echo                  .--,       .--,
-echo                 ( (  \.---./  ) )
-echo                  '.__/o   o\__.'
-echo                     {=  ^^  =}
-echo                      ^>  -  ^<
-echo       ___________.""`-------`"".____________
-echo      /  Hi, the GUI is starting!     O      \
-echo      \                      o               /
-echo      /  .    O       Please wait!       o   \
-echo      \                                      /         __
-echo      /        You can close this window!    \     _.-'  `.
-echo      \______________o__________o____________/ .-~^^        `~--'
-echo                    ___)( )(___        `-.___.'
-echo                   (((__) (__)))
+echo.
+echo.
+echo                   .--,       .--,
+echo                  ( (  \.---./  ) )
+echo                   '.__/o   o\__.'
+echo                      {=  ^^  =}
+echo                       ^>  -  ^<
+echo        ___________.""`-------`"".____________
+echo       /  Hi, the GUI is starting!     O      \
+echo       \                      o               /
+echo       /  .    O       Please wait!       o   \
+echo       \                                      /         __
+echo       /        You can close this window!    \     _.-'  `.
+echo       \______________o__________o____________/ .-~^^        `~--'
+echo                     ___)( )(___        `-.___.'
+echo                    (((__) (__)))
 
 timeout 30 >nul
 exit
