575917dd6de1aac7ee3ab279c326571d4bc1ba55
Alex Piet <alexpiet@gmail.com>
2024-01-31 11:15:59 -0800

    adding ascii art

--- src/desktop_shortcuts/start_popup.bat
+++ src/desktop_shortcuts/start_popup.bat
@@ -1,4 +1,21 @@
-echo Starting the GUI and Bonsai, please wait. This window will close in 20 seconds
-timeout 20 >nul
+echo off
+cls
+echo Starting the GUI and Bonsai, please wait. You can close this window, it will close itself in 20 seconds
+echo             .--,       .--,
+echo            ( (  \.---./  ) )
+echo             '.__/o   o\__.'
+echo                {=  ^  =}
+echo                 >  -  <
+echo  ___________.""`-------`"".____________
+echo /  The GUI is starting!         O      \
+echo \                      o               /
+echo /  .    O                          o   \
+echo \                                      /         __
+echo /                                      \     _.-'  `.
+echo \______________o__________o____________/ .-~^        `~--'
+echo               ___)( )(___        `-.___.'
+echo              (((__) (__)))
 
+timeout 20 >nul
+exit
 
