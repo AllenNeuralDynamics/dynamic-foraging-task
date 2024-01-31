b63f3352657bbca1ef15b7fbabad7eb3c488842e
Alex Piet <alexpiet@gmail.com>
2024-01-31 11:27:33 -0800

    testing

--- src/desktop_shortcuts/start_popup.bat
+++ src/desktop_shortcuts/start_popup.bat
@@ -1,19 +1,20 @@
 echo off
+mode 20,30
 cls
-echo             .--,       .--,
-echo            ( (  \.---./  ) )
-echo             '.__/o   o\__.'
-echo                {=  ^^  =}
-echo                 ^>  -  ^<
-echo  ___________.""`-------`"".____________
-echo /  Hi, the GUI is starting!     O      \
-echo \                      o               /
-echo /  .    O       Please wait!       o   \
-echo \                                      /         __
-echo /        You can close this window!    \     _.-'  `.
-echo \______________o__________o____________/ .-~^^        `~--'
-echo               ___)( )(___        `-.___.'
-echo              (((__) (__)))
+echo                  .--,       .--,
+echo                 ( (  \.---./  ) )
+echo                  '.__/o   o\__.'
+echo                     {=  ^^  =}
+echo                      ^>  -  ^<
+echo       ___________.""`-------`"".____________
+echo      /  Hi, the GUI is starting!     O      \
+echo      \                      o               /
+echo      /  .    O       Please wait!       o   \
+echo      \                                      /         __
+echo      /        You can close this window!    \     _.-'  `.
+echo      \______________o__________o____________/ .-~^^        `~--'
+echo                    ___)( )(___        `-.___.'
+echo                   (((__) (__)))
 
 timeout 30 >nul
 exit
