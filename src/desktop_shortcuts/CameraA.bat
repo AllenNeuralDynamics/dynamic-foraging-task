16e01098565ea2f19aa2866fdb28e4517c7302fd
Alex Piet <alexpiet@gmail.com>
2024-02-05 12:57:43 -0800

    updating camera scripts

--- src/desktop_shortcuts/CameraA.bat
+++ src/desktop_shortcuts/CameraA.bat
@@ -1,3 +1,11 @@
 cd /d C:\Users\svc_aind_behavior\Documents\camera_workflows
-start /min C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxA.bonsai --no-editor 
+echo off
+mode 50,10
+cls
+start /B C:\Users\svc_aind_behavior\Documents\GitHub\dynamic-foraging-task\bonsai\bonsai Camera_boxA.bonsai --no-editor 
+powershell -window minimized -command ""
+timeout 5 > NUL
+title CAMERA A
+echo This window controls camera A
+echo Close this window if camera A is whited out
 
