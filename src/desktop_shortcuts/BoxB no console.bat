632e106df3ecea9eefcb24bf572aaa4430adf4f2
Alex Piet <alexpiet@gmail.com>
2024-02-05 13:10:25 -0800

    adding back the desktop shortcuts that do not open a console

---
+++ src/desktop_shortcuts/BoxB no console.bat
@@ -0,0 +1,5 @@
+cd /d C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
+call conda activate Foraging
+start "" C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\desktop_shortcuts\start_popup.bat
+start "" pythonw Foraging.py 2
+
