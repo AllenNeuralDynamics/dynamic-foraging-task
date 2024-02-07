0ae6a06d9bdf6d4afec7c2cc015c5443bf91eaf6
Alex Piet <alexpiet@gmail.com>
2024-02-07 12:43:41 -0800

    renaming Box with console to configure

--- src/desktop_shortcuts/Configure BoxC.bat
+++ src/desktop_shortcuts/Configure BoxC.bat
@@ -1,6 +1,6 @@
 cd /d C:\Users\%USERNAME%\Documents\GitHub\dynamic-foraging-task\src\foraging_gui
 call conda activate Foraging
-:: This version starts with a console window
-python Foraging.py 1
+python Foraging.py 3
 timeout 3600 > NUL
+:: Open the GUI, the Bonsai IDE, and leave this console open
 
