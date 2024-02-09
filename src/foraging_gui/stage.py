6e9ab940129d00ea41835a45a5b5d073a0ec34fa
Alex Piet <alexpiet@gmail.com>
2024-02-09 10:56:50 -0800

    testing

--- src/foraging_gui/stage.py
+++ src/foraging_gui/stage.py
@@ -88,7 +88,7 @@ class Stage(QObject):
         self.worker.finished.connect(self.thread.quit)
         self.worker.finished.connect(self.worker.deleteLater)
         self.thread.finished.connect(self.thread.deleteLater)
-        self.worker.error.connect(on_error)
+        self.worker.error.connect(self.on_error)
         self.thread.start()
 
         self.z_safe = 0.
