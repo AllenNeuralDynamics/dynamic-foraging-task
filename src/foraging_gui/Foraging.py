0b9c62ac412cb8a0197edc1416712faaf0bac239
Alex Piet <alexpiet@gmail.com>
2024-02-14 16:30:10 -0800

    minor

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2447,11 +2447,6 @@ class Window(QMainWindow):
         # Add note to log
         logging.info('New Session complete')
         return True
-        # TODO, add Save As, which allows to save just the parameters
-        # Save as, if you hit cancel, dont start a new session, just return to previous state
-        # Save as, -> Savecontinue, not start a new session
-        # TODO, Switch Load/Open to disable newSession, and set selected
-
 
     def _AskSave(self):
         reply = QMessageBox.question(self, 'Box {}, New Session:'.format(self.box_letter), 'Do you want to save the current result?',QMessageBox.Yes | QMessageBox.No | QMessageBox.Cancel, QMessageBox.Yes)
