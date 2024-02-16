4803e0592a58d2f6102e2ba7cb9f02606fe7894f
Alex Piet <alexpiet@gmail.com>
2024-02-16 15:40:07 -0800

    testing

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2132,7 +2132,6 @@ class Window(QMainWindow):
         if open_last:
             mice = self._Open_getListOfMice()
             W = MouseSelectorDialog(self, mice)
-            W.exec_()            
 
             ok, mouse_id = (
                 W.exec_() == QtWidgets.QDialog.Accepted, 
