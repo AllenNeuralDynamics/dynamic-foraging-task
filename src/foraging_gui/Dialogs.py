7db22831859d81536b668ddd20b9890e5c47b9a3
Alex Piet <alexpiet@gmail.com>
2024-03-07 13:29:46 -0800

    left justifying strings

--- src/foraging_gui/Dialogs.py
+++ src/foraging_gui/Dialogs.py
@@ -46,17 +46,17 @@ class MouseScheduleDialog(QDialog):
         font.setPointSize(12)
         msg0.setFont(font)
 
-        msg = QLabel('mouse: <span style="color:purple;font-weight:bold">{}</span>'.format(scheduled_mouse['Mouse ID']))
+        msg = QLabel('mouse:'.ljust(8)+'<span style="color:purple;font-weight:bold">{}</span>'.format(scheduled_mouse['Mouse ID']))
         font = msg.font()
         font.setPointSize(12)
         msg.setFont(font)
 
-        msg1 = QLabel('box:   {}'.format(scheduled_mouse['Box']))
+        msg1 = QLabel('box:'.ljust(8)+'{}'.format(scheduled_mouse['Box']))
         font = msg1.font()
         font.setPointSize(12)
         msg1.setFont(font)
 
-        msg2 = QLabel('time:  {}'.format(scheduled_mouse['Time Slot']))
+        msg2 = QLabel('time:'.ljust(8)+'{}'.format(scheduled_mouse['Time Slot']))
         font = msg2.font()
         font.setPointSize(12)
         msg2.setFont(font)
