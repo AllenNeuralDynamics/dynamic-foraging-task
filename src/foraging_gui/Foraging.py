1f421583aca2c5443bd9a6a53a132b16e316efc5
Alex Piet <alexpiet@gmail.com>
2024-03-07 10:37:43 -0800

    typo

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2094,7 +2094,7 @@ class Window(QMainWindow):
         if open_last:
             schedule = self._Open_getSchedule()
             mice = self._Open_getListOfMice()
-            W = MouseSelectorDialog(self, schedule mice)
+            W = MouseSelectorDialog(self, schedule, mice)
 
             ok, mouse_id = (
                 W.exec_() == QtWidgets.QDialog.Accepted, 
