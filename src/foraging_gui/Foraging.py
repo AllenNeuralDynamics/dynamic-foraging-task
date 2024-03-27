157723fe322c47f6078fca11eb2cf5e2307b84fc
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-03-27 14:35:34 -0700

    Added info_performance_essential_2

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2318,7 +2318,8 @@ class Window(QMainWindow):
                     self.label_info_performance_others.setText(Obj['info_other_perf'])
                 if 'info_performance_essential_1' in Obj:
                     self.label_info_performance_essential_1.setText(Obj['info_performance_essential_1'])
-
+                if 'info_performance_essential_2' in Obj:
+                    self.label_info_performance_essential_2.setText(Obj['info_performance_essential_2'])
             elif self.default_ui=='ForagingGUI_Ephys.ui':
                 if 'Other_inforTitle' in Obj:
                     self.infor.setTitle(Obj['Other_inforTitle'])
