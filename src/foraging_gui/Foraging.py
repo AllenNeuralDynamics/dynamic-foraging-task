f51f367c7d9e0bae281b1f9f1e46e99114d035d5
Alex Piet <alexpiet@gmail.com>
2024-04-16 12:11:15 -0700

    testing

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -1358,6 +1358,7 @@ class Window(QMainWindow):
                         if Correct ==0: # incorrect format; don't change
                             child.setText(getattr(Parameters, 'TP_'+child.objectName()))
                         continue
+                    print('here: {}'.format(child.objectName()))
                     if ((child.objectName() in ['PositionX','PositionY','PositionZ','SuggestedWater','BaseWeight','TargetWeight','WeightAfter']) and
                         (child.text() == '')):
                         # These attributes can have the empty string, but we can't set the value as the empty string, unless we allow resets
