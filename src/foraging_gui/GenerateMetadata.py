e0116b646ff7a7001729bb4b372e0ea7db1d360c
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-04-22 13:22:16 -0700

    high level structure

--- src/foraging_gui/GenerateMetadata.py
+++ src/foraging_gui/GenerateMetadata.py
@@ -1,7 +1,28 @@
-
+import json
+import logging
 
 
 class generate_metadata:
-    def __init__(self):
-        self.metadata = {}
+    '''
+    
+    '''
+    def __init__(self,json_file=None,Obj=None):
+        if json_file is None and Obj is None:
+            logging.info("Both json_file and Obj cannot be None")
+            return
+        pass
 
+    def ephys_metadata(self):
+        pass
+    
+    def behavior_metadata(self):
+        pass
+
+    def ophys_metadata(self):
+        pass
+
+    def high_speed_camera_metadata(self):
+        pass
+
+    def combined_metadata(self):
+        pass
