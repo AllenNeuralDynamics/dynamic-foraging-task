ce10a9686376d80f4b67305f672101019a8b34f1
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-04-25 16:03:39 -0700

    finishing __init__

--- src/foraging_gui/GenerateMetadata.py
+++ src/foraging_gui/GenerateMetadata.py
@@ -14,9 +14,11 @@ class generate_metadata:
     dialog_metadata: dict
         dictionary containing the dialog metadata. If provided, it will override the meta_data_dialog key in the json file
     output_folder: str
-        path to the output folder where the metadata will be saved. If not provided, the metadata will be saved in the . 
+        path to the output folder where the metadata will be saved. If not provided, the metadata will be saved in the MetadataFolder extracted from the behavior json file/object. 
 
     Returns:
+    Obj: dict
+        dictionary containing the original dictionary and the updated dictionary with complete metadata
     session_metadata: dict
         dictionary containing the session metadata
     rig_metadata: dict
@@ -29,12 +31,29 @@ class generate_metadata:
         json file to the metadata folder
 
     '''
-    def __init__(self,json_file=None,Obj=None,dialog_metadata=None,output_folder=None):
-        if (json_file is None) and (Obj is None) and (dialog_metadata is None):
+    def __init__(self, json_file=None, Obj=None, dialog_metadata=None, output_folder=None):
+        if json_file is None and Obj is None and dialog_metadata is None:
             logging.info("json file or Obj is not provided")
             return
-        pass
+        
+        if json_file is not None:
+            with open(json_file) as f:
+                self.Obj = json.load(f)
+        else:
+            self.Obj = Obj
 
+        if dialog_metadata is not None:
+            self.Obj['meta_data_dialog'] = dialog_metadata
+        
+        if output_folder is not None:
+            self.Obj['MetadataFolder'] = output_folder
+
+        self.Obj['metadata'] = {}
+        self.Obj['metadata']['session_metadata'] = {}
+        self.Obj['metadata']['rig_metadata'] = self.Obj['meta_data_dialog']['rig_metadata']
+
+        return self.Obj, self.Obj['metadata']['session_metadata'], self.Obj['metadata']['rig_metadata']
+    
     def ephys_metadata(self):
         pass
     
@@ -49,5 +68,3 @@ class generate_metadata:
 
     def combined_metadata(self):
         pass
-
-generate_metadata(json_file=f"metadata.json")
