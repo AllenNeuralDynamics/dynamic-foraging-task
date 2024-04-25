9467de4a8cff64029d7687ec761b990e86e7cd19
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-04-25 15:45:56 -0700

    naming

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -2710,8 +2710,8 @@ class Window(QMainWindow):
                 pass
 
             # load metadata to the metadata dialog
-            if 'meta_data_partial' in Obj:
-                self.Metadata_dialog.meta_data = Obj['meta_data_partial']
+            if 'meta_data_dialog' in Obj:
+                self.Metadata_dialog.meta_data = Obj['meta_data_dialog']
                 self.Metadata_dialog._update_metadata()
 
             # show session list related to that animal
--- src/foraging_gui/GenerateMetadata.py
+++ src/foraging_gui/GenerateMetadata.py
@@ -4,10 +4,33 @@ import logging
 
 class generate_metadata:
     '''
-    
+    Parse the behavior json file and generate session and rig metadata
+
+    Parameters:
+    json_file: str
+        path to the json file
+    Obj: dict
+        dictionary containing the json file
+    dialog_metadata: dict
+        dictionary containing the dialog metadata. If provided, it will override the meta_data_dialog key in the json file
+    output_folder: str
+        path to the output folder where the metadata will be saved. If not provided, the metadata will be saved in the . 
+
+    Returns:
+    session_metadata: dict
+        dictionary containing the session metadata
+    rig_metadata: dict
+        dictionary containing the rig metadata
+
+    Output:
+    session metadata: json file
+        json file to the metadata folder
+    rig metadata: json file
+        json file to the metadata folder
+
     '''
-    def __init__(self,json_file=None,Obj=None):
-        if json_file is None and Obj is None:
+    def __init__(self,json_file=None,Obj=None,dialog_metadata=None,output_folder=None):
+        if (json_file is None) and (Obj is None) and (dialog_metadata is None):
             logging.info("json file or Obj is not provided")
             return
         pass
