c2935bf20041b425c411c7993918387986b31a67
Alex Piet <alexpiet@gmail.com>
2024-05-15 14:21:01 -0700

    adding travel value

--- src/foraging_gui/RigJsonBuilder.py
+++ src/foraging_gui/RigJsonBuilder.py
@@ -110,6 +110,7 @@ def build_rig_json(existing_rig_json, settings, water_calibration, laser_calibra
         stage = d.MotorizedStage(
                     name="AIND lick spout stage",
                     manufacturer=d.Organization.AIND
+                    travel=15.0,
                     )       
     components['stimulus_devices']=[
         d.RewardDelivery(
