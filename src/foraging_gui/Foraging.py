2b7b36fbc849a99ffe800c0c8e80c7709a49d2b5
Alex Piet <alexpiet@gmail.com>
2024-05-15 16:16:01 -0700

    testing

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -1186,7 +1186,7 @@ class Window(QMainWindow):
         rig_settings = self.Settings.copy()
         rig_settings['rig_name'] = self.rig_name 
         rig_settings['box_number'] = self.box_number
-        df = pd.read_csv(self.SettingsBoxFile,index_col=None,header=None).to_dict(orient='series')
+        df = pd.read_csv(self.SettingsBoxFile,index_col=None,header=None)
         df = {row[0]:row[1] for index, row in df.iterrows()}
         print(df)
         rig_settings['box_settings'] = df
