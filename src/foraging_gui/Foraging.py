34db85efea7d510dc298708bacf1f54491054e74
Alex Piet <alexpiet@gmail.com>
2024-06-11 16:21:08 -0700

    testing

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -1248,6 +1248,7 @@ class Window(QMainWindow):
 
         # Log stdout and stderr from bonsai in a separate thread
         threading.Thread(target=log_subprocess_output, args=(process,)).start()
+        threading.Thread(target=log_subprocess_error, args=(process,)).start()
 
 
     def _OpenVideoFolder(self):
@@ -4138,24 +4139,23 @@ class UncaughtHook(QtCore.QObject):
 
 def log_subprocess_output(process):
     logging.info('Bonsai logging starting')
-    while process.poll() is None:
-        inchar = process.stdout.read(1)
-        if inchar:
-            output = process.stdout.readline()
-            logging.info('BONSAI: '+inchar+output.strip())
-        else:
-            logging.info('skipping')
-        #stdout, stderr = process.communicate()
-        #print(stdout)
-        #print('split   ------')
-        #print(stderr)
-        
-        ##output = process.stdout.readline()
-        ##if output:
-        ##    logging.info('BONSAI: '+output.strip())
-        #output = process.stderr.readline()
+    while process.poll() is None:       
+        output = process.stdout.readline()
+        if output:
+            logging.info('BONSAI: '+output.strip())
         time.sleep(.1)
     logging.info('Bonsai logging terminating')
+
+def log_subprocess_error(process):
+    logging.info('Bonsai error logging starting')
+    while process.poll() is None:       
+        output = process.stderr.readline()
+        if output:
+            logging.info('BONSAI: '+output.strip())
+        time.sleep(.1)
+    logging.info('Bonsai error logging terminating')
+
+
 
 if __name__ == "__main__":
 
