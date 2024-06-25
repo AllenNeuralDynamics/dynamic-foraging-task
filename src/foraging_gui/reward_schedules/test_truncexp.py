7235d20a5a210f1c2baef73228c482cfaba175ac
han-lab <houhan@gmail.com>
2024-06-25 15:51:56 -0700

    feat: add overlay plot

--- src/foraging_gui/reward_schedules/test_truncexp.py
+++ src/foraging_gui/reward_schedules/test_truncexp.py
@@ -14,8 +14,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes
 
-def draw_dist_and_harzard(ax_dist, ax_hazard, samples, title):
-    hist, xx, _ = ax_dist.hist(samples, 100, density=True)
+def draw_dist_and_harzard(samples, title, ax_dist, ax_hazard, ax_dist_all, ax_hazard_all, inset_ax_all, color):
+    hist, xx, _ = ax_dist.hist(samples, 100, density=True, color=color)
     hazard = hist / np.flip(np.flip(hist).cumsum())
     
     # Histogram
@@ -27,14 +27,25 @@ def draw_dist_and_harzard(ax_dist, ax_hazard, samples, title):
     ax_dist.legend()
 
     # Hazard function
-    ax_hazard.plot(xx[:-1], hazard)  # Hazard func
+    ax_hazard.plot(xx[:-1], hazard, color=color)  # Hazard func
     ax_hazard.set(ylabel='Hazard rate')
     
     # Zoomed in Hazard function
     inset_ax = inset_axes(ax_hazard, width="40%", height="40%", 
                           loc="upper left", borderpad=4)
-    inset_ax.plot(xx[:-1], hazard)
+    inset_ax.plot(xx[:-1], hazard, color=color)
     inset_ax.set(ylim=(0, 0.1), title='Zoom-in')
+    
+    # Overlay cumulative distribution
+    ax_dist_all.plot(xx[:-1], 
+                     hist.cumsum() / hist.sum(), 
+                     label='TruncExp (redraw)', color=color)
+    
+    # Overlay hazard function
+    ax_hazard_all.plot(xx[:-1], hazard, color=color)  # Hazard func
+    
+    # Zoomed in Hazard function
+    inset_ax_all.plot(xx[:-1], hazard, color=color)
     
     return hist, xx, hazard
 
@@ -58,22 +69,50 @@ def test_truncexp(lower=20,
     uniform = np.random.uniform(lower, upper, n)
 
     # Plotting
-    fig = plt.figure(figsize=(13, 7))
+    fig = plt.figure(figsize=(16, 7))
     fig.clf()
-    ax = fig.subplots(2, 3)
+    ax = fig.subplots(2, 4)
+    
+    inset_ax_all = inset_axes(ax[1, 3], width="40%", height="40%", 
+                              loc="upper left", borderpad=4)
 
-    draw_dist_and_harzard(ax[0, 0], ax[1, 0], 
-                          samples=truncexp_redraw,
-                          title=f'TruncExp (redraw)\nmin={lower}, max={upper}, beta={beta}')
-    draw_dist_and_harzard(ax[0, 1], ax[1, 1], 
-                          samples=truncexp_trunc_at_max,
-                          title=f'TruncExp (trunc_at_max: {truncated_ratio:.2%})\n'
-                          f'min={lower}, max={upper}, beta={beta}')
-    draw_dist_and_harzard(ax[0, 2], ax[1, 2],
-                          samples=uniform,
-                          title=f'Uniform\nmin={lower}, max={upper}')
-
+    draw_dist_and_harzard(
+        samples=truncexp_redraw,
+        title=f'TruncExp (redraw)\nmin={lower}, max={upper}, beta={beta}',
+        ax_dist=ax[0, 0],
+        ax_hazard=ax[1, 0],
+        ax_dist_all=ax[0, 3],
+        ax_hazard_all=ax[1, 3],
+        inset_ax_all=inset_ax_all,
+        color='b',
+        )
+    draw_dist_and_harzard(
+        samples=truncexp_trunc_at_max,
+        title=f'TruncExp (trunc_at_max: {truncated_ratio:.2%})\n'
+        f'min={lower}, max={upper}, beta={beta}',
+        ax_dist=ax[0, 1],
+        ax_hazard=ax[1, 1],
+        ax_dist_all=ax[0, 3],
+        ax_hazard_all=ax[1, 3],
+        inset_ax_all=inset_ax_all,
+        color='g',
+        )
+    draw_dist_and_harzard(
+        samples=uniform,
+        title=f'Uniform\nmin={lower}, max={upper}',
+        ax_dist=ax[0, 2],
+        ax_hazard=ax[1, 2],
+        ax_dist_all=ax[0, 3],
+        ax_hazard_all=ax[1, 3],
+        inset_ax_all=inset_ax_all,
+        color='gray',
+        )
     fig.tight_layout()
+    
+    ax[0, 3].legend()
+    ax[0, 3].set(title='Cumulative distribution')
+    ax[1, 3].set(ylabel='Hazard rate')
+    inset_ax_all.set(ylim=(0, 0.1), title='Zoom-in')
     
     return fig
 
@@ -82,6 +121,6 @@ if __name__ == '__main__':
     test_truncexp(lower=20, upper=60, beta=20)
     test_truncexp(lower=20, upper=80, beta=20)
     test_truncexp(lower=20, upper=100, beta=20)
-    test_truncexp(lower=40, upper=80, beta=20)
+    test_truncexp(lower=20, upper=200, beta=20)
 
     plt.show()
