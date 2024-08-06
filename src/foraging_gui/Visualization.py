905c4e584c4e8d4b1b9b72f20979a95cbe092fdc
Alex Piet <alexpiet@gmail.com>
2024-08-06 14:27:59 -0700

    changing spot check failures to xs

--- src/foraging_gui/Visualization.py
+++ src/foraging_gui/Visualization.py
@@ -452,11 +452,18 @@ class PlotWaterCalibration(FigureCanvas):
                         self.FittingResults[current_date][current_valve]={}
                     self.FittingResults[current_date][current_valve]=[slope,intercept]
                 elif (current_valve in ['SpotLeft','SpotRight'])and(current_date in all_dates):
-                    X,Y=self._GetWaterSpotCheck(self.WaterCalibrationResults,current_date,current_valve)                   
+                    X,Y=self._GetWaterSpotCheck(self.WaterCalibrationResults,current_date,current_valve)      
+                    FAILED = (Y < 2*(1-.15)) or (Y > 2*(1.15))             
                     if current_valve=='SpotLeft':
-                        line=self.ax1.plot(X, Y, 'x',label=current_date+'_spot left')
+                        if FAILED:
+                            line=self.ax1.plot(X, Y, 'x',label=current_date+'_spot left (FAIL)')
+                        else:
+                            line=self.ax1.plot(X, Y, 'o',label=current_date+'_spot left')
                     elif current_valve=='SpotRight':
-                        line=self.ax1.plot(X, Y, 'x',label=current_date+'_spot right')
+                        if FAILED:
+                            line=self.ax1.plot(X, Y, 'x',label=current_date+'_spot right (FAIL)')
+                        else:
+                            line=self.ax1.plot(X, Y, 'o',label=current_date+'_spot right')
         self.ax1.set_xlabel('valve open time(s)')
         self.ax1.set_ylabel('water(mg)')
         self.ax1.legend(loc='lower right', fontsize=8)
