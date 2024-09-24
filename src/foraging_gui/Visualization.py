98b3dba54c3492748cb01c022893d4f92a395ac7
Micah Woodard <micah.woodard@alleninstitute.org>
2024-09-24 14:23:33 -0700

    flipped y axis

--- src/foraging_gui/Visualization.py
+++ src/foraging_gui/Visualization.py
@@ -354,11 +354,15 @@ class PlotV(FigureCanvas):
 
     def _UpdateAxis(self):
         self.ax1.set_yticks([0,1])
-        self.ax1.set_yticklabels(['R', 'L'])
+        self.ax1.set_yticklabels(['L', 'R'])
         self.ax1.set_ylim(-0.6, 1.6)
+        self.ax1.yaxis.set_inverted(True)
+
         self.ax2.set_yticks([0,1])
-        self.ax2.set_yticklabels(['R', 'L'])
+        self.ax2.set_yticklabels(['L', 'R'])
         self.ax2.set_ylim(-0.15, 1.15)
+        self.ax2.yaxis.set_inverted(True)
+        
         if self.main_win.HideLegend.isChecked():
             self.ax1.legend().set_visible(False)
             self.ax2.legend().set_visible(False)
