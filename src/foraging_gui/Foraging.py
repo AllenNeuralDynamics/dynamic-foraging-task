0b507f5f804dd75aa8cdec13e8355937f8dc0ef2
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-12-22 14:32:26 -0800

    adding extra check

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -4670,7 +4670,7 @@ class Window(QMainWindow):
             logging.info('Skipping Automatic Upload based on ForagingSettings.json')
             return
         # skip manifest generation if this is an ephys session
-        if self.open_ephys!=[]:
+        if self.open_ephys!=[] or self.StartEphysRecording.isChecked():
             logging.info('Skipping upload manifest, because this is an ephys session')
             return
 
