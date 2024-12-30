3ac5451c5a2e8e301f7f75692efbf70b36f8e7d3
XX-Yin <109394934+XX-Yin@users.noreply.github.com>
2024-12-30 12:33:02 -0800

    changing the show order

--- src/foraging_gui/Dialogs.py
+++ src/foraging_gui/Dialogs.py
@@ -3172,12 +3172,12 @@ class OpticalTaggingDialog(QDialog):
                 # Emit signal to update the label
                 update_label(
                     f"Cycles: {i+1}/{len(self.current_optical_tagging_par['protocol_sampled_all'])} \n"
+                    f"Color: {laser_color}\n"
+                    f"Laser: {laser_name}\n"
+                    f"Power: {target_power} mW\n"
                     f"protocol: {protocol}\n"
                     f"Frequency: {frequency} Hz\n"
                     f"Pulse Duration: {pulse_duration} ms\n"
-                    f"Laser: {laser_name}\n"
-                    f"Power: {target_power} mW\n"
-                    f"Color: {laser_color}\n"
                     f"Duration: {duration_each_cycle} s\n"
                     f"Interval: {interval_between_cycles} s"
                 )
