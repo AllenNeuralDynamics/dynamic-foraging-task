00e6b6a0ec573e09d00a946d1def3bbc68f088ca
Micah Woodard <micah.woodard@alleninstitute.org>
2025-10-02 08:45:12 -0700

    removes unneeded calculations for foraging eff

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -232,7 +232,7 @@ class Window(QMainWindow):
         self._InitializeBonsai()
 
         # connect to Slims
-        self._ConnectSlims()
+        #self._ConnectSlims()
 
         # Set up threads
         self.threadpool = QThreadPool()  # get animal response
