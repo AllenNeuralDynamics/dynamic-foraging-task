56c199889ee034831e5b63abaa7d62c36c8492c9
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-10-02 19:25:04 -0700

    Update Bonsai to version 2.9

--- bonsai/setup.ps1
+++ bonsai/setup.ps1
@@ -1,9 +1,20 @@
+Push-Location $PSScriptRoot
 if (!(Test-Path "./Bonsai.exe")) {
-    Invoke-WebRequest "https://github.com/bonsai-rx/bonsai/releases/download/2.7.2/Bonsai.zip" -OutFile "temp.zip"
-    Move-Item -Path "NuGet.config" "temp.config"
+    $release = "https://github.com/bonsai-rx/bonsai/releases/latest/download/Bonsai.zip"
+    $configPath = "./Bonsai.config"
+    if (Test-Path $configPath) {
+        [xml]$config = Get-Content $configPath
+        $bootstrapper = $config.PackageConfiguration.Packages.Package.where{$_.id -eq 'Bonsai'}
+        if ($bootstrapper) {
+            $version = $bootstrapper.version
+            $release = "https://github.com/bonsai-rx/bonsai/releases/download/$version/Bonsai.zip"
+        }
+    }
+    Invoke-WebRequest $release -OutFile "temp.zip"
+    Move-Item -Path "NuGet.config" "temp.config" -ErrorAction SilentlyContinue
     Expand-Archive "temp.zip" -DestinationPath "." -Force
-    Move-Item -Path "temp.config" "NuGet.config" -Force
+    Move-Item -Path "temp.config" "NuGet.config" -Force -ErrorAction SilentlyContinue
     Remove-Item -Path "temp.zip"
-    Remove-Item -Path "Bonsai32.exe"
 }
 & .\Bonsai.exe --no-editor
+Pop-Location
