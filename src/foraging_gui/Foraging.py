0493014804dcbdb023723b92aa550b25062975ef
Micah Woodard <micah.woodard@alleninstitute.org>
2026-05-26 10:36:26 -0700

    fixes bug in makeirs

--- src/foraging_gui/Foraging.py
+++ src/foraging_gui/Foraging.py
@@ -373,7 +373,7 @@ class Window(QMainWindow):
     def setup_lifecycle_logger(self):
         
         # Ensure the directory exists
-        os.makedirs(os.path(self.Settings["lifecycle_log_dir"]), exist_ok=True)
+        os.makedirs(Path(self.Settings["lifecycle_log_dir"]), exist_ok=True)
 
         lifecycle_logger = logging.getLogger("lifecycle")
         lifecycle_logger.setLevel(logging.INFO)
